6e5f8054e298d24d58c8f617dd0cef831964303d
Smucek <ajdindjakusic@gmail.com>
2020-09-16 23:51:07 +0200

    Drugi commit

--- src/main/scala/kviz/Program.scala
+++ src/main/scala/kviz/Program.scala
@@ -19,21 +19,15 @@ object Program {
 
       print("Unesite broj pitanja (1-5): ")
       val opcija = scala.io.StdIn.readInt()
-
-      if (opcija == 1) {
-          print(pitanja(opcija - 1), "Odgovor: ")
-          val odgovor = scala.io.StdIn.readLine()
-            if (tacniOdgovori.contains(opcija)) print("To pitanje je vec odgovoreno.")
-            else if (odgovor == odgovori(opcija - 1)) opcija :: tacniOdgovori
-            else { System.exit(1) }
+      if (tacniOdgovori.contains(opcija)) {
+        print("To pitanje je vec odgovoreno.")
       }
-
-
+      else {
+        print(pitanja(opcija - 1), "Odgovor: ")
+        val odgovor = scala.io.StdIn.readLine()
+        if (odgovor == odgovori(opcija - 1)) { tacniOdgovori = tacniOdgovori :+ opcija }
+        else { System.exit(1) }
+      }
     }
-
-
   }
-
-
-
 }
